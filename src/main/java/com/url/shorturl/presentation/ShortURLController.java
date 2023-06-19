975bbaa42833673cfb58b052f37cfa147d196619
= <=>
2023-06-19 12:18:31 +0900

    feat : redirectUrl
    # REST하도록 mapping url 변경

--- src/main/java/com/url/shorturl/presentation/ShortURLController.java
+++ src/main/java/com/url/shorturl/presentation/ShortURLController.java
@@ -10,7 +10,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -21,8 +25,9 @@ public class ShortURLController {
     private final FindUrlProcessor findUrlProcessor;
 
     @ResponseBody
-    @PostMapping("/make/shorturl")
-    public HttpEntity<MakeShortUrlResponse> makeShortUrl(@RequestBody MakeShortUrlRequest makeUrlRequest) {
+    @PostMapping("/shortUrl")
+    public HttpEntity<MakeShortUrlResponse> makeShortUrl(
+        @RequestBody MakeShortUrlRequest makeUrlRequest) {
         log.info("make short url // origin : " + makeUrlRequest.originUrl());
 
         ShortUrlData shortUrlData = shortUrlProcessor.makeUrl(makeUrlRequest);
